34c739eb63f50fe75b7bee7768589fdfa1290283
miguelandin <Miguel.cabrera.landin@gmail.com>
2025-03-27 17:16:47 +0100

    Hanoi Completed

--- Ejer7/Hanoi/Hanoi.cpp
+++ Ejer7/Hanoi/Hanoi.cpp
@@ -1,32 +1,35 @@
-#include<iostream>
-#include"Pila.h"
-#include"assertdomjudge.h"
+#include "Pila.h"
+#include "assertdomjudge.h"
+#include <iostream>
 using namespace std;
 
-void Hanoi(int n,Pila *origen,Pila *destino,Pila *temporal) 
-{
+void Hanoi(int n, Pila* origen, Pila* destino, Pila* temporal)
+{ // T(n) = O(2^n), M(n) = O(n)
+    if (n == 0) // si no quedan discos, terminar
+        return;
 
+    Hanoi(n - 1, origen, temporal, destino); // mueve los n - 1 discos superiores a temp
+    destino->apilar(origen->desapilar()); // se hace el movimiento
+    Hanoi(n - 1, temporal, destino, origen); // mueve los n - 1 discos que están a temp a destino
 }
-
 
 int main()
-{  
-  Pila *A=new Pila("A");
-  Pila *B=new Pila("B");
-  Pila *C=new Pila("C");
+{
+    Pila* A = new Pila("A");
+    Pila* B = new Pila("B");
+    Pila* C = new Pila("C");
 
-  int n;
-  cin>>n;
+    int n;
+    cin >> n;
 
-  for(int i=n;i>0;i--)
-    A->apilar(i);
+    for (int i = n; i > 0; i--)
+        A->apilar(i);
 
-  Hanoi(n,A,C,B);
-  
-  for(int i=0;i<n;i++)
-    C->desapilar();
+    assertdomjudge(n > 0 && A != NULL && B != NULL && C != NULL); // precon
+    Hanoi(n, A, C, B);
 
-  return 0;
-  
+    for (int i = 0; i < n; i++)
+        C->desapilar();
+
+    return 0;
 }
-
--- Ejer7/Hanoi/Pila.cpp
+++ Ejer7/Hanoi/Pila.cpp
@@ -1,30 +1,38 @@
-#include"Pila.h"
+#include "Pila.h"
 
-
-Pila::Pila(string name)
+Pila::Pila(string name) // T(n) = M(n) = O(1)
 {
-
+    this->name = name;
+    this->cima = nullptr;
 }
 
-std::string Pila::nombrePila()
+std::string Pila::nombrePila() // T(n) = M(n) = O(1)
 {
-
+    return this->name;
 }
 
-void Pila::apilar(int num)
+void Pila::apilar(int num) // T(n) = M(n) = O(1)
 {
-  cout<<"Apilando disco "<<num<<" en poste "<<name<<endl;
-}
-  
-int Pila::desapilar()
-{    
+    if (estaVacia()) // se crea la cima actual, su siguiente es el de abajo
+        cima = new Nodo(num, nullptr); // -> el siguiente apunta a null
+    else
+        cima = new Nodo(num, cima); // su siguiente es la antigua cima
 
-  cout<<"Desapilando disco "<<num<<" del poste "<<name<<endl;
-
+    cout << "Apilando disco " << num << " en poste " << name << endl;
 }
 
-bool Pila::estaVacia()
+int Pila::desapilar() // T(n) = M(n) = O(1)
 {
+    Nodo* eliminar = cima; // apunta al nodo a eliminar
+    cima = cima->siguiente; // la cima pasa a ser el de abajo
+    int num = eliminar->valor; // se obtiene el valor de la cima
+    delete eliminar; // elimina el Nodo de la cima
 
+    cout << "Desapilando disco " << num << " del poste " << name << endl;
+    return num;
 }
 
+bool Pila::estaVacia() // T(n) = M(n) = O(1)
+{
+    return cima == nullptr; // si la cima es null, está vacía
+}
