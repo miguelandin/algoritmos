8e9a183435356dbe3cbb6e306662cdb5716c1b11
miguelandin <Miguel.cabrera.landin@gmail.com>
2025-04-02 18:09:39 +0200

    Hash dispersion cerrada completed

---
+++ Ejer8/Hash-dispersion-cerrada/Agenda.cpp
@@ -0,0 +1,111 @@
+#include "Agenda.h"
+#include "assertdomjudge.h"
+
+Agenda::Agenda(int capacidad)
+{
+    assertdomjudge(capacidad > 0); // capacidad tiene que ser natural
+
+    this->capacidad = capacidad; // se guarda la capacidad de la tabla
+    n = 0; // numero actual de elementos a 0
+    /* se reserva memoria de los atributos */
+    nombres = new string[capacidad];
+    telefonos = new long[capacidad];
+    borradas = new bool[capacidad](); // los inicializa a false (no eliminado)
+
+    vacias = new bool[capacidad]; // los inicializa a true (vacio)
+    for (int i = 0; i < capacidad; i++)
+        vacias[i] = true;
+}
+
+Agenda::~Agenda()
+{
+    delete[] nombres;
+    delete[] telefonos;
+    delete[] vacias;
+}
+
+int Agenda::obtenerPosicion(long clave) // T(n) = M(n) = O(1)
+{
+    return clave % capacidad;
+}
+
+int Agenda::buscarContacto(long telefono)
+{
+    int res = -1; // -1 por defecto por si no se encuentra
+    int pos = obtenerPosicion(telefono); // obtenemos la posicion que debería
+    bool termina = false;
+
+    while (!termina) {
+        if (!vacias[pos] && telefonos[pos] == telefono) { // si lo encuentra
+            res = pos; // se guarda el resultado
+            termina = true; // termina el bucle
+        } else if (vacias[pos] && !borradas[pos] || pos == capacidad) // el limite
+            termina = true; // no se ha encontrado, termina el bucle
+        else
+            pos++;
+    }
+
+    return res;
+}
+
+int Agenda::buscarHueco(long telefono)
+{
+    int res = -1;
+    int pos = obtenerPosicion(telefono);
+    bool encontrado = false;
+
+    while (!encontrado && pos < capacidad) { // mientras no se encuentre o pase
+        if (vacias[pos]) { // si no está ocupada se guarda el resultado
+            res = pos;
+            encontrado = true;
+        } else
+            pos++;
+    }
+
+    return res;
+}
+
+bool Agenda::isLlena()
+{
+    return n == capacidad;
+}
+
+bool Agenda::existeContacto(long telefono)
+{
+    if (buscarContacto(telefono) == -1) // si es -1 es que no se encontró
+        return false;
+    else // en otro caso es que se encontró el contacto -> existe
+        return true;
+}
+
+string Agenda::getContacto(long telefono)
+{
+    int pos = buscarContacto(telefono); // se obtiene la posicion del contacto
+    assertdomjudge(pos != -1); // comprueba que exista
+
+    return nombres[pos]; // devuelve el nombre
+}
+
+void Agenda::introducirContacto(long telefono, string contacto)
+{
+    int pos = buscarHueco(telefono); // se obtiene la posicion de un hueco
+    assertdomjudge(pos != -1); // comprueba que se encontró
+
+    telefonos[pos] = telefono; // ponemos el telefono
+    nombres[pos] = contacto; // ponemos el nombre
+    vacias[pos] = false; // ponemos ocupada
+
+    n++; // aumenta el numero de elementos
+}
+
+void Agenda::eliminarContacto(long telefono)
+{
+    int pos = buscarContacto(telefono); // busca la posicion del contacto
+
+    assertdomjudge(pos != -1); // comprueba que se encontró
+
+    vacias[pos] = true; // pasa a estar vacio
+    borradas[pos] = true; // pasa a estar marcada como eliminada
+
+    n--; // disminuye el numero de elementos
+}
--- Ejer8/Hash-dispersion-cerrada/Agenda.h
+++ Ejer8/Hash-dispersion-cerrada/Agenda.h
@@ -3,43 +3,47 @@
 using namespace std;
 
 // Tabla hash que guarda los nombres de los estudiantes de una universidad
-class Agenda
-{
+class Agenda {
 
 protected:
-	int capacidad; // Capacidad de la tabla. Permanece invariante a lo largo de la vida del objeto
+    int capacidad; // Capacidad de la tabla. Permanece invariante a lo largo de la vida del objeto
+    int n; // número actual de elementos
+    // Vectores que guardarán la información de la tabla hash
+    string* nombres; // Vector con los nombres de los contactos
+    bool* vacias; // Vector con los flags que indican si una posición está vacía (true) o no (false). Si esta vacia, tiene basura.
+    bool* borradas; // indica que posiciones han sido eliminadas
+    long* telefonos; // Vector con los telefonos de los contactos
 
-	// Vectores que guardarán la información de la tabla hash
-	string *nombres; // Vector con los nombres de los contactos
-	bool *ocupada;   // Vector con los flags que indican si una posición está vacía (false) o no (true). Si esta vacia, tiene basura.
-	long *telefonos; // Vector con los telefonos de los contactos
-
-	// Método de hashing
-	int obtenerPosicion (long telefono);
+    // Método de hashing
+    int obtenerPosicion(long telefono);
 
 public:
+    // Constructor
+    Agenda(int capacidad);
 
-	// Constructor
-	Agenda(int capacidad);
+    // Comprueba si un contacto está o no en la tabla hash
+    bool existeContacto(long telefono);
 
-	// Comprueba si un contacto está o no en la tabla hash
-	bool existeContacto (long telefono);
+    // Obtiene un contacto (su nombre) a partir de su telefono
+    string getContacto(long telefono);
 
-	// Obtiene un contacto (su nombre) a partir de su telefono
-	string getContacto (long telefono);
+    // Inserta un contacto en la tabla
+    void introducirContacto(long telefono, string contacto);
 
-	// Inserta un contacto en la tabla
-	void introducirContacto (long telefono, string contacto);
+    // Elimina un contacto de la tabla
+    void eliminarContacto(long telefono);
 
-	// Elimina un contacto de la tabla
-	void eliminarContacto (long telefono);
+    // Imprime toda la tabla hash. Viola el principio de separación entre interfaz y modelo
+    void imprimir();
 
-	// Imprime toda la tabla hash. Viola el principio de separación entre interfaz y modelo
-	void imprimir();
+    //Obtiene la posición real de un contacto en la tabla
+    int buscarContacto(long telefono);
 
-	// Destructor
+    //Busca el hueco adecuado para meter el contacto
+    int buscarHueco(long telefono);
 
-	~Agenda();
-
+    //indica si ha llegado la tabla a su máxima capacidad
+    bool isLlena();
+    // Destructor
+    ~Agenda();
 };
-
