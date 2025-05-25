d7971224cec9e508edd7e0934cf32ecc1a57b744
miguelandin <Miguel.cabrera.landin@gmail.com>
2025-05-25 14:35:08 +0200

    completed 'copa-amañada'

--- Concurso-1/Eurocopa-amañada/Eurocopa-amañada.cpp
+++ Concurso-1/Eurocopa-amañada/Eurocopa-amañada.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 char calcular_ganador(char* equipos, int n, int ronda)
 {
-    
+    if (n == 1) { // base case - un solo equipo
+        return equipos[0];
+    }
+
+    int mitad = n / 2;
+    char ganador1 = calcular_ganador(equipos, mitad, ronda + 1);
+    char ganador2 = calcular_ganador(equipos + mitad, mitad, ronda + 1);
+
+    char ganador;
+    if (ronda % 2 == 0)
+        ganador = ganador1;
+    else
+        ganador = ganador2;
+
+    cout << "Ronda " << ronda << " Ganador " << ganador << endl;
+    return ganador;
 }
 
 int main()
@@ -11,8 +26,13 @@ int main()
     int n;
     cin >> n;
     char* equipos = new char[n + 1];
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         equipos[i] = 'A' + i;
-    equipos[n] = 0;
+    }
+    equipos[n] = '\0';
+
     calcular_ganador(equipos, n, 0);
+
+    delete[] equipos; // liberar memoria
+    return 0; // avisar que el programa ha finalizado correctamente
 }
